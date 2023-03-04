b99fdd8eb96969cf165929e4be3f9b3b5c949eaf
Timothy Pendarvis <trpendarvis@gmail.com>
2023-03-03 20:47:59 -0700

    add/ navbar and buttons with images, also added in JS to randomly change the background image

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,26 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+  // I want a function to set a random image as the background from the image array above.
+  document.addEventListener("DOMContentLoaded", function() {
+  const images = ['eva-darron-oCdVtGFeDC0-unsplash.jpg', 'eva-darron-oCdVtGFeDC0-unsplash.jpg', 'dino-reichmuth-A5rCN8626Ck-unsplash.jpg'];
+  let currentIndex = -1;
+
+  function changeImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    // console.log("images???", `url(images/${images[currentIndex]})`);
+    const imgUrl = `url(images/${images[currentIndex]})`;
+    document.getElementById("background-image").style.backgroundImage = imgUrl;
+  }
+  changeImage();
+  setInterval(changeImage, 3000);
+  })
+
+
+
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
