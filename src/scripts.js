87e2b7b8cd9e430034e7342cfed524eb95590194
Timothy Pendarvis <trpendarvis@gmail.com>
2023-03-04 19:21:07 -0700

    add/navbar and back ground and functions for images

--- src/scripts.js
+++ src/scripts.js
@@ -2,24 +2,28 @@
 // Do not delete or rename this file ********
 
   // I want a function to set a random image as the background from the image array above.
-  document.addEventListener("DOMContentLoaded", function() {
-  const images = ['eva-darron-oCdVtGFeDC0-unsplash.jpg', 'eva-darron-oCdVtGFeDC0-unsplash.jpg', 'dino-reichmuth-A5rCN8626Ck-unsplash.jpg'];
-  let currentIndex = -1;
-
-  function changeImage() {
+document.addEventListener("DOMContentLoaded", function() {
+const images = ['eva-darron-oCdVtGFeDC0-unsplash.jpg', 'eva-darron-oCdVtGFeDC0-unsplash.jpg', 'dino-reichmuth-A5rCN8626Ck-unsplash.jpg', 'ross-parmly-rf6ywHVkrlY-unsplash.png', 'robert-lukeman-zNN6ubHmruI-unsplash.jpg', 'pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg', 'jack-anstey-XVoyX7l9ocY-unsplash.jpg'];
+let currentIndex = 0;
+function changeImage() {
     currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
+    if(currentIndex >= images.length) {
+    currentIndex = 0;
     }
-    // console.log("images???", `url(images/${images[currentIndex]})`);
-    const imgUrl = `url(images/${images[currentIndex]})`;
-    document.getElementById("background-image").style.backgroundImage = imgUrl;
-  }
+const imgUrl = `url(images/${images[currentIndex]})`;
+document.getElementById("background-image").style.backgroundImage = imgUrl;
+}
   changeImage();
-  setInterval(changeImage, 3000);
-  })
+  setInterval(changeImage, 10000);
+})
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    const datepicker = document.getElementById('datepicker');
+    const datepickerConfig = {dateFormat: 'mm/dd/yy'};
+    new DatePicker(datepicker, datepickerConfig);
+  });
+  
+  
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
