d3396f8e71c7a607261452378c40afcf84798733
Timothy Pendarvis <trpendarvis@gmail.com>
2023-03-05 12:51:35 -0700

    added in datepicker and have it displayed to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,17 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+// An example of how you tell webpack to use a CSS (SCSS) file
+import './css/styles.css';
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './images/turing-logo.png'
+
 //I want to be able to keep the right container hidden from view until the user searches I am using CSS visibility: hidden
 // document.querySelector('.containerRight').classList.add('hidden');
 // document.querySelector('.containerRight').classList.remove('hidden');
 
 
-  // I want a function to set a random image as the background from the image array above.
+  //function to set a random image as the background from the image array above.
 document.addEventListener("DOMContentLoaded", function() {
 const images = ['eva-darron-oCdVtGFeDC0-unsplash.jpg', 'eva-darron-oCdVtGFeDC0-unsplash.jpg', 'dino-reichmuth-A5rCN8626Ck-unsplash.jpg', 'ross-parmly-rf6ywHVkrlY-unsplash.png', 'robert-lukeman-zNN6ubHmruI-unsplash.jpg', 'pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg', 'jack-anstey-XVoyX7l9ocY-unsplash.jpg'];
 let currentIndex = 4;
@@ -22,10 +27,8 @@ document.getElementById("background-image").style.backgroundImage = imgUrl;
   setInterval(changeImage, 300000);
 })
 
+//this is the datepicker
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
 
-
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
